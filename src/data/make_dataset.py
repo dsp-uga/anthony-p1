8d781077643c43598717de24cbb84d78ebe8cbd6
nathanWynn <nww15119@uga.edu>
2021-02-15 16:33:15 -0500

    added starter code to make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,19 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pyspark
+import sys
+# main entry point for Spark
+# SparkContext represents connection to a Spark cluster
+spark_conf = pyspark.SparkConf().setMaster("local[*]").setAppName("p0")
+sc = pyspark.SparkContext(conf = spark_conf)
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+if len(sys.argv) != 3:
+  raise Exception("Exactly 2 arguments are required: <inputUri> <outputUri>")
+
+inputUri=sys.argv[1]
+outputUri=sys.argv[2]
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+lines = sc.textFile(sys.argv[1])
+words = lines.flatMap(lambda line: line.split())
+wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda count1, count2: count1 + count2)
+wordCounts.saveAsTextFile(sys.argv[2])
--- src/models/model.py
+++ src/models/model.py
@@ -4,3 +4,19 @@ import pyspark
 # SparkContext represents connection to a Spark cluster
 spark_conf = pyspark.SparkConf().setMaster("local[*]").setAppName("p0")
 sc = pyspark.SparkContext(conf = spark_conf)
+
+
+
+# TODO
+# 1. Preprocessing (bag of words, feature set)
+# 2. Naive Bayes
+# 3. Work on other models
+
+def preprocess():
+    # TODO
+
+def naive_bayes():
+    # TODO
+
+def random_forest():
+    # TODO
